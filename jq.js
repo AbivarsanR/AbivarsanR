9db6404d5f4e352d7d3057eda82e4391b8be5135
AbivarsanR <abivarsanr4104@gmail.com>
2024-06-24 11:18:29 +0530

    Add files via upload
    git ready

---
+++ jq.js
@@ -0,0 +1,54 @@
+
+alert("Click the baby green dragen")
+
+        $ ("#img1").hide();
+        $ ("#img2").hide();
+        $("#img3").hide();
+        $ ("#img1").fadeToggle(100);
+        $("#img2").fadeToggle(700)
+        $("#img3").fadeToggle(2000)
+       
+       
+       $("#f").hide();
+       $("#s").hide();
+       $("#t").hide();
+       $("#fo").hide();
+       $("#fi").hide();
+       $("#si").hide();
+       $("#se").hide();
+       $("#ei").hide();
+       $("#ni").hide();
+       $("#in").hide();       
+    
+       $("#f").show(2000);
+       $("#s").show(2500);
+       $("#t").show(3000);
+       $("#fo").show(3500);
+       $("#fi").show(4000);
+       $("#si").show(4500);
+       $("#se").show(5000);
+       $("#ei").show(5500);
+       $("#ni").show(6000); 
+       $("#in").show(6500);      
+    
+       
+        
+        
+    
+        //$("body").css("background-color","green");
+        
+        //function fun(){
+
+            /*tr = document.getElementById("tree").value;
+    
+           for (let i = 0; i <100; i++) 
+            {
+                const nu = document.createElement("trf");
+            
+            document.getElementById("trf").innerText=nu;
+        }
+    
+    }*/
+
+    
+
